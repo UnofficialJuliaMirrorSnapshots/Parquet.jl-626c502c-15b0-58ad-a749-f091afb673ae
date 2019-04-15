57dffaa00a63dd56d9cff1513a38f107336aaf13
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-15 01:34:51 +0000

    Snapshot of branch master taken on 2019-04-14T21:34:50.818-04:00 from "https://github.com/UnofficialJuliaMirror/Parquet.jl-626c502c-15b0-58ad-a749-f091afb673ae"

---
+++ src/Parquet.jl
@@ -0,0 +1,28 @@
+module Parquet
+
+using Thrift
+using ProtoBuf
+using Snappy
+using CodecZlib
+using MemPool
+using Compat
+using Nullables
+
+import Base: show, open, close, values
+import Thrift: isfilled
+
+export is_par_file, ParFile, show, nrows, ncols, rowgroups, columns, pages, bytes, values, colname, colnames
+export SchemaConverter, schema, JuliaConverter, ThriftConverter, ProtoConverter
+export RowCursor, ColCursor, RecCursor
+export AbstractBuilder, JuliaBuilder
+
+# package code goes here
+include("PAR2/PAR2.jl")
+using .PAR2
+include("codec.jl")
+include("schema.jl")
+include("reader.jl")
+include("cursor.jl")
+include("show.jl")
+
+end # module
